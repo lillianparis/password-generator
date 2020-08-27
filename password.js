d7a907a3d4eefc5e4612c4a2f30c9dd2d364239f
Lillian Paris <lillian.paris529@gmail.com>
2020-08-26 23:03:09 -0700

    Changed script.js to password.js

---
+++ password.js
@@ -0,0 +1,38 @@
+
+// Greeting
+
+alert("Welcome! Let's create a password.");
+
+// Creating arrays using var https://www.w3schools.com/js/js_arrays.asp 
+
+let numberValue = "0123456789";
+let specialCharector = "!@#$%^&*()_+";
+let lowerCase =  "abcdefghijklmnopqrstuvwxyz"; 
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+// Interactivity
+let passLength = prompt("Choose a password length between 8-128 charectors");
+
+if (passLength < 8 || passLength > 128) {
+    alert("Please choose a whole number between 8 and 128 charectors");
+}
+if (confirm("Would you like to use upper case letters?")) {
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
