3c26bda1e7581476bc9d3c54a8dce352331d7a58
Lillian Paris <lillian.paris529@gmail.com>
2020-08-28 21:29:45 -0700

    Added finishing comments to the password.js

--- password.js
+++ password.js
@@ -1,6 +1,4 @@
-
-
-// Creating arrays using let https://www.w3schools.com/js/js_arrays.asp 
+// Creating arrays using let. A helpful source I founf using Google-Fu.. https://www.w3schools.com/js/js_arrays.asp 
 
 let numberValue = "0123456789";
 let specialCharector = "!@#$%^&*()_+";
@@ -10,11 +8,21 @@ let possibleSelection = "";
 let passLength;
 let generateBtn = document.getElementById("generate");
 let passwordDisplay = document.getElementById("password");
-// Interactivity, promt asks user the length of the password in charectors
-// The if statement sees if the user chooses a number between 8-128 charectors, if not then it prompts user to retry
+// Interactivity, promt asks user the length of the password in charectors.
+
+// The if statement sees if the user chooses a number between 8-128 charectors, if not then it prompts user to retry.
+
 function promptUser() {
-// parseInt changes to a number
+    
+// parseInt changes to a number.
+
 passLength = parseInt(prompt("Choose a password length between 8-128 charectors"));
+
+// If the user does not choose a password between 8 and 128, they will receive another prompt to choose a number.
+
+// Possible selection = the password being generated.
+
+// Each line includes possible selection with each of the four options being added or subtracted from it.
 
 if (passLength < 8 || passLength > 128) {
     alert("Please choose a whole number between 8 and 128 charectors");
@@ -31,17 +39,17 @@ if (confirm("Would you like to use special charectors?")){
 if (confirm("Would you like to use number values?")){
     possibleSelection = possibleSelection + numberValue
 }
-// function stops running
+// function stops running when 'return' prompt is inserted at the end.
 return
 }
+// Gives user option to click button, then prompts.
 
 generateBtn.addEventListener("click", function() {
     promptUser()
     generatePassword()
 })
 
-
-
+// This function allows the code to run using the layout in the style.css and index.html
 
 function generatePassword() {
     let password = "";
